7554f261d3e83270d671b585b742a3d8e4857aec
mudasir Nadeem <mudasirnadeem7979@gmail.com>
2024-02-29 21:19:02 +0500

    Commit

--- list.js
+++ list.js
@@ -1,16 +1,16 @@
 var addcontent = document.getElementById('addcontent');
 var index=1;
 function listdata() {
-    var inputbox = document.getElementById('inputbox').value;
+    var inputBoxValue = document.getElementById('inputbox').value;
     index++;
-    if(inputbox.length > 0){
+    if(inputBoxValue.length > 0){
         addcontent.innerHTML += `
         <div id=${index} class="d-flex my-2">
-        <a href="#" class="list-group-item d-flex align-items-center justify-content-between list-group-item-action"  onclick="overline(${index})">${inputbox}
+        <a href="#" class="list-group-item d-flex align-items-center justify-content-between list-group-item-action"  onclick="overline(${index})">${inputBoxValue}
         </a>
         <button class="btn btn-primary border-0 rounded-0" onclick="deleteFun(${index})"><i class="fa fa-trash-o"></i></button>
     </div>`;
-     inputbox = document.getElementById('inputbox').value = "";
+    inputBoxValue = document.getElementById('inputbox').value = "";
     }
     else{
         alert("Please Enter a data");
